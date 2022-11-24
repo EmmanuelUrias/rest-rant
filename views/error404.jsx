91f8efeb6bd43efa1bbbec1507e93abee47a2da2
EmmanuelU <emmanuelurias60@icloud.com>
2022-11-23 19:48:18 -0800

    added image to home

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,10 @@ function error404 () {
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p>Oops, sorry, we can't find this page!</p>
+            <img src="" alt=""></img>
+            <div>
+                Photo by <a></a> on <a></a>
+            </div>
         </main>
     </Default>
    )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Default>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="public/images/sanjeevan-satheeskumar-MG8c-4n1QVE-unsplash.jpg" alt="Tree"></img>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@evannn_n">Sanjeevan SatheesKumar </a> on <a href="https://unsplash.com/s/photos/free">Unsplash</a>
+                    </div>
+                </div>
             </main>
             <a href="/places">
                 <button className="btn-primary">Places Page</button>
