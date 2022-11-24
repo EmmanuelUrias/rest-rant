5feb7be8ad727407aa0fb82484370d6a8f0ea639
EmmanuelU <emmanuelurias60@icloud.com>
2022-11-23 20:49:38 -0800

    new places can be added but not displayed

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
     let places = [{
@@ -24,7 +25,17 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        req.body.pic = 'https://placekitten.com/250/250'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 module.exports = router
